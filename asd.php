1ecb4dca40d640556ad2f65aba45b5f0cbecd278
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-04-28 00:53:28 +0600

    first commit

---
+++ asd.php
@@ -0,0 +1,17 @@
+<?php
+$x ="code love me";
+$y ="I love to code";
+
+// echo $x;
+
+$a = "$x $y";
+// echo $a;
+
+echo substr ($x ,4);
+
+// echo  trim("   code is ok   ")
+// echo $x + $y;
+
+// $z =explode (" " , $y);
+// print_r ($z)
+?>

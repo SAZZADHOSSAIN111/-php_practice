c3bb6ffce9055b3769e51a8ef91e8809f45a9c88
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-05-03 15:32:37 +0600

    php constants and magice constant

--- asd.php
+++ asd.php
@@ -24,5 +24,32 @@ echo $math = (string) $math; //php casting
 var_dump($math);
 // echo(round($math));
 echo "<br>";
-echo(abs($math))
+echo(abs($math));
+echo "<br>";
+
+// php constant
+
+define("web3", "1st define practiced");
+echo web3;
+echo "<br>";
+
+const define = "define with const";
+echo define;
+
+echo "<br>";
+// php magic constants
+
+echo "this is in line number:" . __LINE__;
+echo "<br>";
+echo "this file from:" . __FILE__;
+echo "<br>";
+echo "this is the directory fo this file:" . __DIR__;
+echo "<br>";
+
+function robinhood(){
+    echo "this function name is:" . __FUNCTION__;
+
+}
+
+robinhood();
 ?>

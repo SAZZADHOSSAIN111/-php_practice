32f05a5867a4a05018c4f7f94ba6c9c1e75034b8
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-05-04 14:46:47 +0600

    git pull

--- asd.php
+++ asd.php
@@ -11,15 +11,17 @@ var_dump(is_string($x));
 echo "<br>";
 echo substr ($x ,4);
 
-// echo  trim("   code is ok   ")
+// echo  trim("code is ok")
 // echo $x + $y;
 
 // $z =explode (" " , $y);
 // print_r ($z)
 
 $math = -6.50;
-
+echo "<br>";
 echo $math = (string) $math; //php casting
+
+// php math
 
 var_dump($math);
 // echo(round($math));

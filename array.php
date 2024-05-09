fd3f4b1be43fee4c0ec0d0de820986e49385f82c
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-05-10 00:27:37 +0600

    php create array, acces array, update array, add array item, remove array and sorting array

--- array.php
+++ array.php
@@ -37,6 +37,53 @@ foreach($detailed as $x=>$y){
 }
 var_dump($detailed);
 echo $detailed["model"];
+echo "<br>";
 
+function arrayFunc() {
+    echo "xpeodfsjalksdjflaksjd";
+}
+$funcarray = array("a", "b", "message"=> "arrayFunc");
 
+$funcarray["message"]();
+echo "<br>";
+$cars=array("brand"=>"bmw", "model"=>"M2", "age"=>2024);
+
+foreach($cars as $x=>$y){
+    echo "$x:$y <br>";
+}
+$cars3=array("brand", "model", "age");
+unset($cars3[0], $cars3[1]);
+// array_splice($cars3, 1, 2);
+var_dump($cars3);
+echo "<br>";
+$cars4=array("brand"=>"bmw", "model"=>"M2", "age"=>2024);
+unset ($cars4["brand"], $cars4["model"]);
+var_dump($cars4);
+echo "<br>";
+
+$cars5=array("brand", "model", "age");
+sort($cars5);
+$ccars=count($cars5);
+for ($t=0; $t<$ccars; $t++){
+    echo "$cars5[$t]";
+    echo "<br>";
+}
+
+$number = array(8, 9, 1, 2 , 3, 4, 5);
+sort($number);
+$numbers= count($number);
+for($r=0; $r<$numbers; $r++){
+    echo "$number[$r] <br>";
+}
+$number = array(8, 9, 1, 2 , 3, 4, 5);
+rsort($number);
+$numbers= count($number);
+for($r=0; $r<$numbers; $r++){
+    echo "$number[$r] <br>";
+}
+$cars6=array("brand"=>"Bmw", "model"=>"Z2", "year"=>2024);
+asort($cars6);
+foreach ($cars6 as $r=>$title){
+    echo "$r:$title <br>";
+}
 ?>

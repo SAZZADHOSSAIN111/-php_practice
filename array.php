399102250c858eda0485d2cc724aa7f02c29cb20
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-05-10 21:58:40 +0600

    php multdimensional aray and array function

--- array.php
+++ array.php
@@ -82,8 +82,20 @@ for($r=0; $r<$numbers; $r++){
     echo "$number[$r] <br>";
 }
 $cars6=array("brand"=>"Bmw", "model"=>"Z2", "year"=>2024);
-asort($cars6);
+krsort($cars6);
 foreach ($cars6 as $r=>$title){
     echo "$r:$title <br>";
 }
+
+$multi = array(
+    array("rich" , 22342, 2344),
+    array("poor" , 22434, 4456),  
+    array("gorib" , 232, 4454), 
+    array("faul" , 232, 4546), 
+);
+echo $multi[0][0]." is the status ". $multi[0][1]. " is have money " . $multi[0][2]. " loan <br>";
+echo $multi[1][0]." is the status ". $multi[1][1]. " is have money " . $multi[1][2]. " loan <br>";
+echo $multi[2][0]." is the status ". $multi[2][1]. " is have money " . $multi[2][2]. " loan <br>";
+echo $multi[3][0]." is the status ". $multi[3][1]. " is have money " . $multi[3][2]. " loan <br>";
+
 ?>

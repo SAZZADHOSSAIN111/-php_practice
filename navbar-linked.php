e66b5b1f1a904ef0f4deb448b6f0e1f7a698eda3
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-05-14 16:19:37 +0600

    php include, require, handling

---
+++ navbar-linked.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php require 'navbar.php'; echo "<br>";
+        readfile("navbar.php"); //php file handling
+        ?>
+    </div>
+<!-- 
+    <form action="post">
+        <label for="firstname">Firstname</label> <br>
+        <input type="text" name="firstname">
+
+        <button>Submit</button>
+    </form>
+    <div>
+    </div> -->
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+
+<?php
+echo '<a href="/default.asp">Home</a> -
+<a href="/html/default.asp">HTML Tutorial</a> -
+<a href="/css/default.asp">CSS Tutorial</a> -
+<a href="/js/default.asp">JavaScript Tutorial</a> -
+<a href="default.asp">PHP Tutorial</a>';
+echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+
+$color='red';
+$car='BMW';
+
+echo "$color, $car";
+
+?>
